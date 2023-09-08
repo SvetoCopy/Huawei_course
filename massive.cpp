a33f07992c2f2dc1f60a83ff5b136815db0b7f2d
SvetoCopy <mrfairow@yandex.com>
2023-09-08 18:36:36 +0300

    4.0 version

--- massive.cpp
+++ massive.cpp
@@ -7,14 +7,16 @@
 
 char** ReadFile(const char* name) {
     char* buff = (char*)calloc(200, sizeof(char));
+    size_t nLines = 10;
     FILE* f = NULL;
-    char* data[10];
+    char** data = (char**)calloc(nLines, sizeof(char*));
 
     fopen_s(&f, name, "r");
     assert(f != NULL);
 
     size_t i = 0;
     while (fgets(buff, 100, f) != NULL) {
+        
         data[i] = _strdup(buff);
         i++;
     }
@@ -27,4 +29,7 @@ int main()
     const char* name = "text.txt";
  
     char** txt = ReadFile(name);
+    for (int i = 0; i < 4; ++i) {
+        printf("%s", txt[i]);
+    }
 }
